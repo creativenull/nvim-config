0ba63d01704da9411c8866655ceb6c4e14612d52
Arnold Chand <creativenull@outlook.com>
2021-09-29 12:30:49 -0400

    feat(plugins/core): use nvim-tree, autopairs

--- lua/cnull/plugins/core.lua
+++ lua/cnull/plugins/core.lua
@@ -2,19 +2,14 @@ local M = {
   plugins = {
     -- Deps
     {'nvim-lua/plenary.nvim'},
-    {'nvim-lua/popup.nvim'},
-    {'antoinemadec/FixCursorHold.nvim', opt = true},
-    {'lambdalisue/nerdfont.vim',  opt = true},
-    {'lambdalisue/fern-renderer-nerdfont.vim', opt = true},
     {'vim-denops/denops.vim'},
 
     -- Core
-    {'steelsojka/pears.nvim'},
+    {'windwp/nvim-autopairs'},
     {'creativenull/projectlocal-vim'},
     {'editorconfig/editorconfig-vim'},
     {'kevinhwang91/nvim-bqf'},
-    {'mcchrish/nnn.vim', opt = true},
-    {'lambdalisue/fern.vim', opt = true},
+    {'kyazdani42/nvim-tree.lua'},
     {'tpope/vim-abolish', opt = true},
     {'tpope/vim-surround', opt = true},
     {'tpope/vim-repeat', opt = true},
@@ -22,56 +17,33 @@ local M = {
 }
 
 function M.before()
-  vim.g.projectlocal = {
-    projectConfig = '.vim/init.lua',
+  vim.g.projectlocal = { projectConfig = '.vim/init.lua' }
+
+  -- nvim-tree.lua Config
+  -- ---
+  vim.g.nvim_tree_show_icons = {
+    git = 0,
+    folders = 1,
+    files = 1,
   }
 end
 
 function M.after()
   local nmap = require('cnull.core.keymap').nmap
 
-  -- nnn.vim/fern.vim Config
+  -- nvim-tree.lua Config
   -- ---
-  function _G.NnnLoad()
-    if not vim.g['g:nnn#loaded'] then
-      vim.cmd('packadd nnn.vim')
-      require('nnn').setup({
-        set_default_mappings = false,
-        layout = {
-          window = {
-            width = 0.9,
-            height = 0.6,
-            highlight = 'Debug',
-          },
-        },
-      })
-    end
+  require('nvim-tree').setup({
+    view = {
+      side = 'right',
+    },
+  })
 
-    vim.cmd('NnnPicker %:p:h')
-  end
+  nmap('<Leader>ff', '<Cmd>NvimTreeToggle<CR>')
 
-  function _G.FernLoad()
-    vim.g['fern#renderer'] = 'nerdfont'
-
-    if not vim.g.loaded_fern and not vim.g.loaded_fix_cursorhold_nvim then
-      vim.cmd('packadd FixCursorHold.nvim')
-      vim.cmd('packadd fern.vim')
-      vim.cmd('packadd nerdfont.vim')
-      vim.cmd('packadd fern-renderer-nerdfont.vim')
-    end
-
-    vim.cmd('Fern . -reveal=%')
-  end
-
-  if vim.fn.has('win32') == 1 then
-    nmap('<Leader>ff', '<Cmd>lua FernLoad()<CR>')
-  else
-    nmap('<Leader>ff', '<Cmd>lua NnnLoad()<CR>')
-  end
-
-  -- pears.nvim Config
+  -- nvim-autopairs Config
   -- ---
-  require('pears').setup()
+  require('nvim-autopairs').setup({})
 end
 
 return M
