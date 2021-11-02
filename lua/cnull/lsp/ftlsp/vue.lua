47dcf1838c2e57dee8a7b80b4f8638d97c8d6ce8
Arnold Chand <creativenull@outlook.com>
2021-11-02 16:18:35 -0400

    feat(lsp/vue): combine vls and volar

--- lua/cnull/lsp/ftlsp/vue.lua
+++ lua/cnull/lsp/ftlsp/vue.lua
@@ -1,48 +1,73 @@
-local exec = 'vls'
-if vim.fn.executable(exec) == 0 then
-  vim.api.nvim_err_writeln(string.format('lsp: %q is not installed', exec))
+local vls_exec = 'vls'
+if vim.fn.executable(vls_exec) == 0 then
+  vim.api.nvim_err_writeln(string.format('lsp: %q is not installed', vls_exec))
+  return
+end
+
+local volar_exec = 'volar-server'
+if vim.fn.executable(volar_exec) == 0 then
+  vim.api.nvim_err_writeln(string.format('lsp: %q is not installed', volar_exec))
   return
 end
 
 local root_pattern = require('lspconfig').util.root_pattern
-require('cnull.core.lsp').setup('vuels', {
-  root_dir = root_pattern('package.json', 'vue.config.js'),
-  init_options = {
-    config = {
-      css = {},
-      emmet = {},
-      html = {
-        suggest = {},
-      },
-      javascript = {
-        format = {},
-      },
-      stylusSupremacy = {},
-      typescript = {
-        format = {},
-      },
-      vetur = {
-        completion = {
-          autoImport = false,
-          tagCasing = 'kebab',
-          useScaffoldSnippets = false,
+
+-- Vue 2 Project
+local vue = {}
+vue.root = vim.fn.getcwd() .. '/vue.config.js'
+vue.is_project = vim.fn.filereadable(vue.root) == 1
+vue.root_patterns = root_pattern('vue.config.js')
+
+-- Vue 3 Project
+local vue_next = {}
+vue_next.root = vim.fn.getcwd() .. '/vite.config.js'
+vue_next.is_project = vim.fn.filereadable(vue_next.root) == 1
+vue_next.root_patterns = root_pattern('vite.config.js')
+
+if vue.is_project then
+  require('cnull.core.lsp').setup('vuels', {
+    root_dir = vue.root_patterns,
+    init_options = {
+      config = {
+        css = {},
+        emmet = {},
+        html = {
+          suggest = {},
         },
-        format = {
-          defaultFormatter = {
-            js = 'none',
-            ts = 'none',
+        javascript = {
+          format = {},
+        },
+        stylusSupremacy = {},
+        typescript = {
+          format = {},
+        },
+        vetur = {
+          completion = {
+            autoImport = false,
+            tagCasing = 'kebab',
+            useScaffoldSnippets = false,
           },
-          defaultFormatterOptions = {},
-          scriptInitialIndent = false,
-          styleInitialIndent = false,
-        },
-        useWorkspaceDependencies = false,
-        validation = {
-          script = true,
-          style = true,
-          template = true,
+          format = {
+            defaultFormatter = {
+              js = 'none',
+              ts = 'none',
+            },
+            defaultFormatterOptions = {},
+            scriptInitialIndent = false,
+            styleInitialIndent = false,
+          },
+          useWorkspaceDependencies = false,
+          validation = {
+            script = true,
+            style = true,
+            template = true,
+          },
         },
       },
     },
-  },
-})
+  })
+elseif vue_next.is_project then
+  require('cnull.core.lsp').setup('volar', {
+    root_dir = vue_next.root_patterns,
+  })
+end
