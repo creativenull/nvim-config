f5d3a14286cc92eeccb34fe9ead063305515634b
Arnold Chand <creativenull@outlook.com>
2021-12-31 11:28:31 -0400

    feat(plugins/autocompletion): switch to cmp

--- lua/cnull/plugins/autocompletion.lua
+++ lua/cnull/plugins/autocompletion.lua
@@ -1,39 +1,40 @@
 local M = {
   plugins = {
     -- nvim-cmp
-    -- { 'hrsh7th/nvim-cmp' },
-    -- { 'hrsh7th/cmp-nvim-lsp' },
-    -- { 'hrsh7th/cmp-vsnip' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-vsnip' },
+    { 'hrsh7th/cmp-buffer' },
 
     -- coq_nvim
-    { 'ms-jpq/coq_nvim', branch = 'coq' },
-    { 'ms-jpq/coq.artifacts', branch = 'artifacts' },
+    -- { 'ms-jpq/coq_nvim', branch = 'coq' },
+    -- { 'ms-jpq/coq.artifacts', branch = 'artifacts' },
 
     -- ddc.vim
-    -- {'Shougo/ddc.vim'},
-    -- {'Shougo/ddc-sorter_rank'},
-    -- {'matsui54/ddc-matcher_fuzzy'},
-    -- {'Shougo/ddc-around'},
-    -- {'Shougo/ddc-nvim-lsp'},
-    -- {'matsui54/ddc-ultisnips'},
-    -- {'matsui54/ddc-nvim-lsp-doc'},
+    -- { 'Shougo/ddc.vim' },
+    -- { 'Shougo/ddc-sorter_rank' },
+    -- { 'matsui54/ddc-matcher_fuzzy' },
+    -- { 'Shougo/ddc-around' },
+    -- { 'Shougo/ddc-nvim-lsp' },
+    -- { 'matsui54/ddc-ultisnips' },
+    -- { 'matsui54/ddc-nvim-lsp-doc' },
   },
 }
 
 function M.before()
   -- coq_nvim Config
   -- ---
-  require('cnull.plugins.autocompletions.coq').before()
+  -- require('cnull.plugins.autocompletions.coq').before()
 end
 
 function M.after()
   -- nvim-cmp Config
   -- ---
-  -- require('cnull.plugins.autocompletions.cmp')
+  require('cnull.plugins.autocompletions.cmp')
 
   -- coq_nvim Config
   -- ---
-  require('cnull.plugins.autocompletions.coq').after()
+  -- require('cnull.plugins.autocompletions.coq').after()
 
   -- ddc.vim Config
   -- ---
