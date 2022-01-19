e035bb4b923a7791a187835f7c98be463019e414
Arnold Chand <creativenull@outlook.com>
2022-01-18 23:12:37 -0400

    feat(plugins/lsp): load keymaps once

--- lua/cnull/plugins/lsp.lua
+++ lua/cnull/plugins/lsp.lua
@@ -9,18 +9,24 @@ local M = {
 
 function M.after()
   local nmap = require('cnull.core.keymap').nmap
+  vim.g.lsp_keymaps_loaded = false
 
   local function on_attach(_, bufnr)
     local diag_opts = '{ width = 80, border = "rounded" }'
 
-    -- Keymaps
-    nmap('<Leader>la', '<Cmd>lua vim.lsp.buf.code_action()<CR>', { bufnr = bufnr })
-    nmap('<Leader>ld', '<Cmd>lua vim.lsp.buf.definition()<CR>', { bufnr = bufnr })
-    nmap('<Leader>le', '<Cmd>lua vim.diagnostic.setloclist()<CR>', { bufnr = bufnr })
-    nmap('<Leader>lf', '<Cmd>lua vim.lsp.buf.formatting()<CR>', { bufnr = bufnr })
-    nmap('<Leader>lh', '<Cmd>lua vim.lsp.buf.hover()<CR>', { bufnr = bufnr })
-    nmap('<Leader>lr', '<Cmd>lua vim.lsp.buf.rename()<CR>', { bufnr = bufnr })
-    nmap('<Leader>lw', '<Cmd>lua vim.diagnostic.open_float(' .. diag_opts .. ')<CR>', { bufnr = bufnr })
+    if not vim.g.lsp_keymaps_loaded then
+      vim.g.lsp_keymaps_loaded = true
+
+      -- Load LSP Keymaps once
+      nmap('<Leader>la', '<Cmd>lua vim.lsp.buf.code_action()<CR>', { bufnr = bufnr })
+      nmap('<Leader>ld', '<Cmd>lua vim.lsp.buf.definition()<CR>', { bufnr = bufnr })
+      nmap('<Leader>le', '<Cmd>lua vim.diagnostic.setloclist()<CR>', { bufnr = bufnr })
+      nmap('<Leader>lf', '<Cmd>lua vim.lsp.buf.formatting()<CR>', { bufnr = bufnr })
+      nmap('<Leader>lh', '<Cmd>lua vim.lsp.buf.hover()<CR>', { bufnr = bufnr })
+      nmap('<Leader>lr', '<Cmd>lua vim.lsp.buf.rename()<CR>', { bufnr = bufnr })
+      nmap('<Leader>lw', '<Cmd>lua vim.diagnostic.open_float(' .. diag_opts .. ')<CR>', { bufnr = bufnr })
+    end
+
   end
 
   local nvimlsp = require('cnull.core.lsp')
