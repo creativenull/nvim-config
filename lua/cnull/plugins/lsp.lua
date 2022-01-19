b4ab01387003dbb91e2b0bd14585f29e231f6e21
Arnold Chand <creativenull@outlook.com>
2022-01-18 23:27:07 -0400

    fix(plugins/lsp): reg once doesn't work, it's buf specific

--- lua/cnull/plugins/lsp.lua
+++ lua/cnull/plugins/lsp.lua
@@ -9,24 +9,18 @@ local M = {
 
 function M.after()
   local nmap = require('cnull.core.keymap').nmap
-  vim.g.lsp_keymaps_loaded = false
 
   local function on_attach(_, bufnr)
     local diag_opts = '{ width = 80, border = "rounded" }'
 
-    if not vim.g.lsp_keymaps_loaded then
-      vim.g.lsp_keymaps_loaded = true
-
-      -- Load LSP Keymaps once
-      nmap('<Leader>la', '<Cmd>lua vim.lsp.buf.code_action()<CR>', { bufnr = bufnr })
-      nmap('<Leader>ld', '<Cmd>lua vim.lsp.buf.definition()<CR>', { bufnr = bufnr })
-      nmap('<Leader>le', '<Cmd>lua vim.diagnostic.setloclist()<CR>', { bufnr = bufnr })
-      nmap('<Leader>lf', '<Cmd>lua vim.lsp.buf.formatting()<CR>', { bufnr = bufnr })
-      nmap('<Leader>lh', '<Cmd>lua vim.lsp.buf.hover()<CR>', { bufnr = bufnr })
-      nmap('<Leader>lr', '<Cmd>lua vim.lsp.buf.rename()<CR>', { bufnr = bufnr })
-      nmap('<Leader>lw', '<Cmd>lua vim.diagnostic.open_float(' .. diag_opts .. ')<CR>', { bufnr = bufnr })
-    end
-
+    -- Load LSP Keymaps once
+    nmap('<Leader>la', '<Cmd>lua vim.lsp.buf.code_action()<CR>', { bufnr = bufnr })
+    nmap('<Leader>ld', '<Cmd>lua vim.lsp.buf.definition()<CR>', { bufnr = bufnr })
+    nmap('<Leader>le', '<Cmd>lua vim.diagnostic.setloclist()<CR>', { bufnr = bufnr })
+    nmap('<Leader>lf', '<Cmd>lua vim.lsp.buf.formatting()<CR>', { bufnr = bufnr })
+    nmap('<Leader>lh', '<Cmd>lua vim.lsp.buf.hover()<CR>', { bufnr = bufnr })
+    nmap('<Leader>lr', '<Cmd>lua vim.lsp.buf.rename()<CR>', { bufnr = bufnr })
+    nmap('<Leader>lw', '<Cmd>lua vim.diagnostic.open_float(' .. diag_opts .. ')<CR>', { bufnr = bufnr })
   end
 
   local nvimlsp = require('cnull.core.lsp')
