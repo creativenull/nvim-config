11d9aefd8e6e7c905e3aff58ec66cf082fb1ccd9
Arnold Chand <creativenull@outlook.com>
2022-02-18 12:48:05 -0400

    feat(plugins/lsp): refactor, don't register any lsp servers

--- lua/cnull/plugins/lsp.lua
+++ lua/cnull/plugins/lsp.lua
@@ -10,26 +10,35 @@ local M = {
 function M.after()
   local nmap = require('cnull.core.keymap').nmap
 
-  local function on_attach(_, bufnr)
-    local diag_opts = '{ width = 80, border = "rounded" }'
-
-    -- Load LSP Keymaps once
+  local function set_keymaps(bufnr)
+    -- LSP Keymaps
     nmap('<Leader>la', '<Cmd>lua vim.lsp.buf.code_action()<CR>', { bufnr = bufnr })
     nmap('<Leader>ld', '<Cmd>lua vim.lsp.buf.definition()<CR>', { bufnr = bufnr })
     nmap('<Leader>le', '<Cmd>lua vim.diagnostic.setloclist()<CR>', { bufnr = bufnr })
     nmap('<Leader>lf', '<Cmd>lua vim.lsp.buf.formatting()<CR>', { bufnr = bufnr })
     nmap('<Leader>lh', '<Cmd>lua vim.lsp.buf.hover()<CR>', { bufnr = bufnr })
     nmap('<Leader>lr', '<Cmd>lua vim.lsp.buf.rename()<CR>', { bufnr = bufnr })
-    nmap('<Leader>lw', '<Cmd>lua vim.diagnostic.open_float(' .. diag_opts .. ')<CR>', { bufnr = bufnr })
+
+    local diagopts = '{ width = 80, border = "rounded" }'
+    nmap(
+      '<Leader>lw',
+      string.format('<Cmd>lua vim.diagnostic.open_float(%d, %s)<CR>', bufnr, diagopts),
+      { bufnr = bufnr }
+    )
   end
+
+  local function on_attach(_, bufnr)
+    set_keymaps(bufnr)
+  end
+
+  -- LSP info keymap
+  nmap('<Leader>li', '<Cmd>LspInfo<CR>')
 
   local nvimlsp = require('cnull.core.lsp')
   nvimlsp.init({
     -- debug = true,
   })
   nvimlsp.on_attach = on_attach
-
-  require('cnull.lsp').setup({ 'javascript', 'json', 'typescript', 'vue', 'lua', 'python' })
 
   local dlsconfig = require('diagnosticls-configs')
   dlsconfig.init({ on_attach = on_attach })
