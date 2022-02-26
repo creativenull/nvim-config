6d16a0fe1b0d704de85e24d7377b60a3341e37ab
Arnold Chand <creativenull@outlook.com>
2022-02-26 16:01:24 -0400

    feat(user): add custom colors to be used for tabline/statusline

---
+++ lua/cnull/user/colors.lua
@@ -0,0 +1,31 @@
+local base_colors = {
+  light = '#f1f5f9',
+  dark = '#111827',
+  softlight = '#cbd5e1',
+  softdark = '#334155',
+}
+
+return {
+  text_light = base_colors.light,
+  text_dark = base_colors.dark,
+  text_softlight = base_colors.softlight,
+  text_softdark = base_colors.softdark,
+
+  bg_light = base_colors.light,
+  bg_dark = base_colors.dark,
+  bg_softlight = base_colors.softlight,
+  bg_softdark = base_colors.softdark,
+
+  primary = '#0369a1',
+  indigo = '#4f46e5',
+  gray = '#1f2937',
+  darkgray = '#111827',
+
+  insert_primary = '#0c4a6e',
+  visual_primary = '#92400e',
+  command_primary = '#94a3b8',
+  replace_primary = '#94a3b8',
+
+  error = '#881337',
+  warn = '#fdba74',
+}
