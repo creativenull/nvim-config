e1e23218ebfefd55c97b23c935d2d38b26b2ad38
Arnold Chand <creativenull@outlook.com>
2022-02-26 15:49:49 -0400

    feat(user/options): proper checks for win32

--- lua/cnull/user/options.lua
+++ lua/cnull/user/options.lua
@@ -1,17 +1,17 @@
 -- Windows specific
-if vim.fn.has('win32') then
-local shellcmdflag = {
-  '-NoLogo',
-  '-NoProfile',
-  '-ExecutionPolicy RemoteSigned',
-  '-Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
-}
-vim.opt.shell = 'pwsh'
-vim.opt.shellcmdflag = table.concat(shellcmdflag, ' ')
-vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-vim.opt.shellquote= ''
-vim.opt.shellxquote= ''
+if vim.fn.has('win32') == 1 then
+  local shellcmdflag = {
+    '-NoLogo',
+    '-NoProfile',
+    '-ExecutionPolicy RemoteSigned',
+    '-Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;',
+  }
+  vim.opt.shell = 'pwsh'
+  vim.opt.shellcmdflag = table.concat(shellcmdflag, ' ')
+  vim.opt.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+  vim.opt.shellquote= ''
+  vim.opt.shellxquote= ''
 end
 
 -- Automatically create the undo directory
