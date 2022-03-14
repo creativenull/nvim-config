3ac195879ab72e8499605b35dc33d4741dd3922b
Arnold Chand <creativenull@outlook.com>
2022-03-14 13:17:33 -0400

    feat(plugins/lsp): modify server capabilities

--- lua/cnull/plugins/lsp.lua
+++ lua/cnull/plugins/lsp.lua
@@ -10,8 +10,10 @@ local M = {
 function M.after()
   local nmap = require('cnull.core.keymap').nmap
 
+  ---Set keymaps when an LSP server attaches to the nvim client
+  ---@param bufnr number
+  ---@return nil
   local function set_keymaps(bufnr)
-    -- LSP Keymaps
     nmap('<Leader>la', '<Cmd>lua vim.lsp.buf.code_action()<CR>', { bufnr = bufnr })
     nmap('<Leader>ld', '<Cmd>lua vim.lsp.buf.definition()<CR>', { bufnr = bufnr })
     nmap('<Leader>le', '<Cmd>lua vim.diagnostic.setloclist()<CR>', { bufnr = bufnr })
@@ -27,7 +29,26 @@ function M.after()
     )
   end
 
-  local function on_attach(_, bufnr)
+  ---Disable formatting from some servers
+  ---@param client any
+  ---@return nil
+  local function modify_server(client)
+    local servers = { 'tsserver', 'sumneko_lua' }
+
+    for _,server in pairs(servers) do
+      if client.name == server and client.resolved_capabilities.document_formatting then
+        client.resolved_capabilities.document_formatting = false
+      end
+    end
+  end
+
+  ---LSP on_attach to set configurations that are specific to the each LSP
+  ---server
+  ---@param client any
+  ---@param bufnr number
+  ---@return nil
+  local function on_attach(client, bufnr)
+    modify_server(client)
     set_keymaps(bufnr)
   end
 
