1a35a173932a270ba4ac52b28b1fb00db1a503dc
Arnold Chand <creativenull@outlook.com>
2022-03-14 12:29:09 -0400

    feat(plugins/finder): rm commented code

--- lua/cnull/plugins/finder.lua
+++ lua/cnull/plugins/finder.lua
@@ -32,33 +32,4 @@ function M.after()
   -- require('cnull.plugins.finders.fzf').after()
 end
 
---[[ if _G.CNull.config.finder == 'telescope' then
-  M.plugins = { {'nvim-telescope/telescope.nvim'} }
-  M.after = function()
-    pcall(require, 'cnull.plugins.finders.telescope')
-  end
-elseif _G.CNull.config.finder == 'snap' then
-  M.plugins = { {'camspiers/snap'} }
-  M.after = function()
-    pcall(require, 'cnull.plugins.finders.snap')
-  end
-else
-  M.plugins = {
-    {'junegunn/fzf'},
-    {'junegunn/fzf.vim'},
-  }
-  M.before = function()
-    local success, fzf = pcall(require, 'cnull.plugins.finders.fzf')
-    if success then
-      fzf.before()
-    end
-  end
-  M.after = function()
-    local success, fzf = pcall(require, 'cnull.plugins.finders.fzf')
-    if success then
-      fzf.after()
-    end
-  end
-end ]]
-
 return M
