d94afdd93cae3e08ffcdc2eeadc58dc29394ada5
Arnold Chand <creativenull@outlook.com>
2022-04-21 18:20:32 -0400

    feat(ftplugins): omit comment.nvim, for now

--- after/ftplugin/javascript.lua
+++ after/ftplugin/javascript.lua
@@ -2,9 +2,6 @@ vim.cmd('packadd vim-abolish')
 vim.cmd('packadd vim-repeat')
 vim.cmd('packadd vim-surround')
 
-vim.cmd('packadd Comment.nvim')
-require('Comment').setup()
-
 vim.cmd('packadd indent-blankline.nvim')
 require('cnull.plugins.ui.indent_blankline')
 
